fc0b34c94d7f3de7f0ad3905ea5a8b6eb2a49307
Imdad <imdadsemail@gmail.com>
2026-01-13 22:41:04 +0530

    Document generation feature compeleted

--- app/controllers/generated_documents_controller.rb
+++ app/controllers/generated_documents_controller.rb
@@ -24,6 +24,7 @@ class GeneratedDocumentsController < ApplicationController
     @generated_document = GeneratedDocument.new(generated_document_params)
     puts "#####Parameters: #{generated_document_params}"
     @generated_document.placeholder_inputs = generated_document_params["placeholder_inputs"].to_json
+    @generated_document.generate_the_document(generated_document_params["template_id"])
 
     respond_to do |format|
       if @generated_document.save
--- app/models/generated_document.rb
+++ app/models/generated_document.rb
@@ -1,3 +1,61 @@
+require 'docx'
 class GeneratedDocument < ApplicationRecord
   belongs_to :template
+  has_one_attached :file
+
+  def generate_the_document(template_id)
+
+    template_doc = Template.find_by(id: template_id).template_document
+    raise "Template document not found" unless template_doc&.attached?
+
+    Tempfile.create(["template_file", ".docx"]) do |template_doc_file|
+      template_doc_file.binmode
+      template_doc_file.write(template_doc.download)
+      template_doc_file.flush
+      template_doc_file.rewind
+
+      doc = Docx::Document.open(template_doc_file.path)
+
+      # Replace placeholder in paragraphs
+      doc.paragraphs.each do |paragraph|
+        paragraph.each_text_run do |run|
+          JSON.parse(placeholder_inputs || '{}').each do |key, value|
+            pattern = /\{\{[^#]+##{Regexp.escape(key)}\}\}/
+            run.substitute(pattern, value.to_s)
+          end
+        end
+      end
+
+      # Replace placeholder inside tables
+      doc.tables.each do |table|
+        table.rows.each do |row|
+          row.cells.each do |cell|
+            cell.paragraphs.each do |paragraph|
+              paragraph.each_text_run do |run|
+                JSON.parse(placeholder_inputs || '{}').each do |key, value|
+                  pattern = /\{\{[^#]+##{Regexp.escape(key)}\}\}/
+                  run.substitute(pattern, value.to_s)
+                end
+              end
+            end
+          end
+        end
+      end
+
+      # Save generated document to a new file
+      Tempfile.create(["generated_document", ".docx"]) do |output_file|
+        doc.save(output_file.path)
+        output_file.rewind
+
+        # Attach it to the active storage field of this model
+        self.file.attach(
+          io: File.open(output_file.path, 'rb'),
+          filename: "#{name}.docx",
+          content_type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+
+        self.save!
+      end
+    end
+  end
 end
