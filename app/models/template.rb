1e77d6a0f5cc21e69121c694b01ed56a50acbfa2
Imdad <imdadsemail@gmail.com>
2026-01-23 01:44:03 +0530

    Added after commit - update callback method to enqueue sidekiq job

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,4 +1,13 @@
 class Template < ApplicationRecord
   has_one_attached :template_document
-  
+
+  after_commit :extract_placeholders, on: [:update]
+
+  private
+
+  def extract_placeholders()
+    return unless self.template_document.attached?
+    ExtractPlaceholders.perform_async(self.id)
+  end
+
 end
