07d7653f93220daaa08d522aa1d7e8c89a59e9e3
Imdad <imdadsemail@gmail.com>
2026-05-03 20:53:58 +0530

    Search will happen on change instead of pressing Enter key

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -3,8 +3,20 @@ import{Controller}from"@hotwired/stimulus";
 export default class extends Controller{
     
     submit(event){
+
         if(event.key === "Enter"){
             this.element.closest("form").requestSubmit();
       }
     }
+
+    submit_on_date_change(){
+        this.element.closest("form").requestSubmit();
+    }
+
+    submitWithDelay() {
+    clearTimeout(this.timer)
+    this.timer = setTimeout(() => {
+      this.element.closest("form").requestSubmit();
+    }, 400)
+  }
 }
