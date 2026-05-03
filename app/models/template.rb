7b40a3056bec30d15471060d3b9fa80e43189da9
Imdad <imdadsemail@gmail.com>
2026-05-03 21:19:19 +0530

    added scope search_filter

--- app/models/template.rb
+++ app/models/template.rb
@@ -5,6 +5,16 @@ class Template < ApplicationRecord
 
   scope :for_user, -> (user) {where(user_id: user.id)}
 
+  scope :search_filter, -> (filters) {
+  scope =  all 
+  scope = scope.where("lower(name) LIKE ?", "%#{filters[:name].downcase}%") if filters[:name].present?
+  scope = scope.where("cast(id as text) = ?", filters[:id]) if filters[:id].present?
+  scope = scope.where("date(created_at) = ?", filters[:created_at]) if filters[:created_at].present?
+  scope = scope.where("date(updated_at) = ?", filters[:updated_at]) if filters[:updated_at].present?
+  scope = scope.joins(template_document_attachment: :blob).where("lower(active_storage_blobs.filename) LIKE ?", "%#{filters[:attachment_name].downcase}%") if filters[:attachment_name].present?
+  scope
+}
+
   after_commit :extract_placeholders, on: [:update]
 
   private
